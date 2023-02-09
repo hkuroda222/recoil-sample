12d54aef08234a59b7fd9c327caa8b51c0dbbf7d
HirokiKuroda <kinaco.ao@gmail.com>
2023-02-09 22:04:24 +0900

    recoilの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
 import React from 'react';
+import { RecoilRoot } from 'recoil';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        <h1>Learning Recoil</h1>
+      </div>
+    </RecoilRoot>
   );
 }
 
